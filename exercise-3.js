3ffacdd00360106b0465c73de64d6b022ced038f
Rizky Ichsandi <icsandirizky@gmail.com>
2019-07-22 20:43:49 +0700

    add exercise 3

--- exercise-3.js
+++ exercise-3.js
@@ -1,18 +1,22 @@
 function makanTerusRekursif(waktu) {
   // you can only write your code here!
+  // init variable count to return and ramining to be parameters for iteration
   var count = 0;
   var remaining = 0;
 
+  // BASE CASES
   if (waktu === 0) {
     return 0;
   } else if (waktu > 0 && waktu < 15) {
     return 1;
-  } else {
-
+  } 
+  // for every iteraion count added by 1 and waktu minus by 15
+  else {
     count++;
     remaining = waktu -15;
   }
 
+  // return count and call function with remaining
   return count + makanTerusRekursif(remaining);
 }
 
